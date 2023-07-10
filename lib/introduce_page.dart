f015a6fe16562d7f4495ca8ed6f67126f6511e59
PotatoMeme <kshk0897@gmail.com>
2023-07-10 21:05:13 +0900

    [DESIGN] : 🎨 소개페이지 디자인

--- lib/introduce_page.dart
+++ lib/introduce_page.dart
@@ -31,25 +31,130 @@ class IntroducePage extends StatelessWidget {
           )
         ],
       ),
-      body: Column(
-        children: [
-          OutlinedButton(
-            onPressed: () {},
-            child: Text("소개수정페이지로 이동"),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(currentHumman.thumbUrl),
+                        radius: 75.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          currentHumman.name,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "직책 : ${currentHumman.posittion} / 취미 : ${currentHumman.hobby} / mbti : ${currentHumman.mbti}",
+                        maxLines: 5,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(8.0),
+                  color: Color.fromARGB(255, 217, 221, 244),
+                  child: Row(
+                    children: [
+                      Text(
+                        "블로그 주소",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                        ),
+                        maxLines: 5,
+                      ),
+                      Flexible(
+                          child: TextButton(
+                        child: Text(
+                          currentHumman.blogUrl,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => WebViewPage(
+                                      url: currentHumman.blogUrl,
+                                    )),
+                          );
+                        },
+                      )),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "스타일",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding:
+                      EdgeInsets.only(left: 8, right: 8, top: 16, bottom: 16),
+                  color: Color.fromARGB(255, 217, 221, 244),
+                  child: Text(currentHumman.style),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "장점",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding:
+                      EdgeInsets.only(left: 8, right: 8, top: 16, bottom: 16),
+                  color: Color.fromARGB(255, 217, 221, 244),
+                  child: Text(currentHumman.advantages),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "목표",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding:
+                      EdgeInsets.only(left: 8, right: 8, top: 16, bottom: 16),
+                  color: Color.fromARGB(255, 217, 221, 244),
+                  child: Text(currentHumman.goal),
+                ),
+              ],
+            ),
           ),
-          OutlinedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (_) => WebViewPage(
-                          url: currentHumman.blogUrl,
-                        )),
-              );
-            },
-            child: Text("웹뷰페이지로 이동"),
-          ),
-        ],
+        ),
       ),
     );
   }
