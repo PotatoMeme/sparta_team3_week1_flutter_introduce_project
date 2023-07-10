ba4246626e43c4a48e5e5502b93d807860a72741
PotatoMeme <kshk0897@gmail.com>
2023-07-10 19:20:00 +0900

    [FEAT] : ⚙️ WebView 구현

--- lib/introduce_page.dart
+++ lib/introduce_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sparta_team3_week1_flutter_introduce_project/human.dart';
 import 'package:sparta_team3_week1_flutter_introduce_project/introduce_edit_page.dart';
 import 'package:sparta_team3_week1_flutter_introduce_project/webview_page.dart';
 
@@ -8,9 +9,25 @@ class IntroducePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Human currentHumman = Human(
+        name: "홍길동",
+        posittion: "팀원",
+        hobby: "독서",
+        blogUrl: "https://seedpotato.tistory.com/",
+        style: "저는 이러한 스타일을 가지고 있습니다",
+        advantages: "저는 이러한 장점을 가지고 있습니다.",
+        mbti: "AAAA",
+        goal: "좋은 개발자",
+        thumbUrl:
+            "https://avatars.akamai.steamstatic.com/d18545996fbbc474a47e76c566c8a9ac7b09e8d0_full.jpg");
     return Scaffold(
       appBar: AppBar(
-        title: Text("소개 페이지"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.edit),
+          )
+        ],
       ),
       body: Column(
         children: [
@@ -27,7 +44,10 @@ class IntroducePage extends StatelessWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (_) => WebViewPage()),
+                MaterialPageRoute(
+                    builder: (_) => WebViewPage(
+                          url: currentHumman.blogUrl,
+                        )),
               );
             },
             child: Text("웹뷰페이지로 이동"),
