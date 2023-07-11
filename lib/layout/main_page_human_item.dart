fa18c75a0ff8bfdbc51ca7e485c8c6f43efa1ebd
SeoJungHan <seojh5939@gmail.com>
2023-07-11 11:59:14 +0900

    [DESIGN] : 🎨 회의실 -> 카드형으로 수정완

--- lib/layout/main_page_human_item.dart
+++ lib/layout/main_page_human_item.dart
@@ -1,41 +1,35 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sparta_team3_week1_flutter_introduce_project/introduce_page.dart';
 
 /// 메인페이지에서 사용되는 사람 얼굴 Item
 class MainPageHumanItem extends StatelessWidget {
   MainPageHumanItem({
     Key? key,
-    required this.name,
     required this.path,
+    required this.name,
+    required this.position,
+    required this.mbti,
+    required this.func,
   }) : super(key: key);
-  String name;
   String path;
+  String name;
+  String position;
+  String mbti;
+  void Function()? func;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (_) => IntroducePage()),
-        );
-      },
+      onTap: func,
       child: Container(
-        alignment: Alignment.topCenter,
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            alignment: Alignment.bottomCenter,
-            image: AssetImage(path),
-          ),
-        ),
-        child: Text(
-          name,
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+        child: Card(
+          elevation: 1,
+          color: Colors.white,
+          child: Column(
+            children: [
+              Image.asset(path, scale: 0.5),
+              Text("$name / $position / $mbti"),
+            ],
           ),
         ),
       ),
