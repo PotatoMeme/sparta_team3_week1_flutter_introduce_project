c326fdbd2f76be84f51d79cc20f78130015aca17
PotatoMeme <kshk0897@gmail.com>
2023-07-12 19:53:24 +0900

    [DOCS] : 📚 introduce edit 주석 추가

--- lib/introduce_edit_page.dart
+++ lib/introduce_edit_page.dart
@@ -5,14 +5,19 @@ import 'package:provider/provider.dart';
 import 'package:sparta_team3_week1_flutter_introduce_project/human.dart';
 import 'package:sparta_team3_week1_flutter_introduce_project/introduce_service.dart';
 
+//소개를 수정하는 페이지 입니다.
 class IntroduceEditPage extends StatelessWidget {
+  //몇번째 값을 가져와야하는지 알아야하기때문에 생성자에서 idx값을 받아옵니다.
   const IntroduceEditPage({super.key, required this.idx});
   final int idx;
 
   @override
   Widget build(BuildContext context) {
+    ////소개를 추가할려면 서비스를 가져와야하므로 context.read로 서비스를 가져옵니다.
     IntroduceService introduceService = context.read<IntroduceService>();
     Human currentHuman = introduceService.humanList[idx];
+    //text의 입력을 TextEditingController를 통해 introduceService로 전달해줘야 하므로 정의해줍니다
+    //또한 위에서 지정한 currentHuman의 값들을 컨틀롤러의 text에 넣어줘서 기존의 값들을넣어 줍니다.
     TextEditingController textEditingControllerName =
         TextEditingController(text: currentHuman.name);
     TextEditingController textEditingControllerPosition =
@@ -44,12 +49,14 @@ class IntroduceEditPage extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
+              //저장버튼을 누를 경우의 로직임
               //저장로직을 구현해야함
               showUpdateDialog(
                   context,
                   () => {
                         introduceService.updateHuman(
                             index: idx,
+                            //입력을 바탕으로 Human을 새로 만들어 updateHuman함수를 통하여 기존의 idx값의 내용을 바꿔줍니다.
                             changed: Human(
                                 name: textEditingControllerName.text,
                                 posittion: textEditingControllerPosition.text,
@@ -85,7 +92,8 @@ class IntroduceEditPage extends StatelessWidget {
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  controller: textEditingControllerName,
+                  controller:
+                      textEditingControllerName, //항목에 맞게 TextEditingController를 집어넣어 줍니다
                 ),
               ),
               Text(
@@ -101,7 +109,8 @@ class IntroduceEditPage extends StatelessWidget {
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  controller: textEditingControllerPosition,
+                  controller:
+                      textEditingControllerPosition, //항목에 맞게 TextEditingController를 집어넣어 줍니다
                 ),
               ),
               Text(
@@ -117,7 +126,8 @@ class IntroduceEditPage extends StatelessWidget {
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  controller: textEditingControllerHobby,
+                  controller:
+                      textEditingControllerHobby, //항목에 맞게 TextEditingController를 집어넣어 줍니다
                 ),
               ),
               Text(
@@ -133,7 +143,8 @@ class IntroduceEditPage extends StatelessWidget {
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  controller: textEditingControllerBlogUrl,
+                  controller:
+                      textEditingControllerBlogUrl, //항목에 맞게 TextEditingController를 집어넣어 줍니다
                 ),
               ),
               Text(
@@ -149,7 +160,8 @@ class IntroduceEditPage extends StatelessWidget {
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  controller: textEditingControllerStyle,
+                  controller:
+                      textEditingControllerStyle, //항목에 맞게 TextEditingController를 집어넣어 줍니다
                 ),
               ),
               Text(
@@ -165,7 +177,8 @@ class IntroduceEditPage extends StatelessWidget {
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  controller: textEditingControllerAdvantages,
+                  controller:
+                      textEditingControllerAdvantages, //항목에 맞게 TextEditingController를 집어넣어 줍니다
                 ),
               ),
               Text(
@@ -181,7 +194,8 @@ class IntroduceEditPage extends StatelessWidget {
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  controller: textEditingControllerMBTI,
+                  controller:
+                      textEditingControllerMBTI, //항목에 맞게 TextEditingController를 집어넣어 줍니다
                 ),
               ),
               Text(
@@ -197,7 +211,8 @@ class IntroduceEditPage extends StatelessWidget {
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  controller: textEditingControllerGoal,
+                  controller:
+                      textEditingControllerGoal, //항목에 맞게 TextEditingController를 집어넣어 줍니다
                 ),
               ),
               Text(
@@ -213,7 +228,8 @@ class IntroduceEditPage extends StatelessWidget {
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  controller: textEditingControllerThumUrl,
+                  controller:
+                      textEditingControllerThumUrl, //항목에 맞게 TextEditingController를 집어넣어 줍니다
                 ),
               ),
             ],
@@ -236,7 +252,7 @@ class IntroduceEditPage extends StatelessWidget {
                 //예버튼을 누를경우의 로직
                 Navigator.pop(context); //다이얼로그를 끕니다.
                 Navigator.pop(context); //페이지를 끕니다.
-                dialogClosed();
+                dialogClosed(); //예버튼을 누를경우 위에 입력한  dialogClosed를 호출해줍니다
               },
               child: Text("예"),
             ),
