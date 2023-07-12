df8bd2c3f570be3ed7d48b2c0902a614f8b5edc1
PotatoMeme <kshk0897@gmail.com>
2023-07-12 15:52:30 +0900

    [DOCS] : 📚 public thing && human 주석 추가

--- lib/public_thing.dart
+++ lib/public_thing.dart
@@ -8,6 +8,7 @@ class PublicThing {
   String value; // 내용
 
   Map toJson() {
+    //현재값을 map으로 변환
     return {
       "name": name,
       "value": value,
@@ -15,6 +16,7 @@ class PublicThing {
   }
 
   factory PublicThing.fromJson(json) {
+    //map값을 이용하여 객체를 생성
     return PublicThing(
       name: json["name"],
       value: json["value"],
