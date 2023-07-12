2b85f38e55b8cea65991c3414d6a141230584c08
PotatoMeme <kshk0897@gmail.com>
2023-07-12 15:49:56 +0900

    [DOCS] : 📚 webview page 주석 추가

--- lib/webview_page.dart
+++ lib/webview_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewPage extends StatelessWidget {
-  const WebViewPage({super.key, required this.url});
+  //WebViewPage입니다
+  const WebViewPage({super.key, required this.url}); //생성자로 url을 받아 옵니다
 
   final String url;
 
@@ -13,7 +14,7 @@ class WebViewPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("웹뷰 페이지"),
       ),
-      body: WebView(initialUrl: url),
+      body: WebView(initialUrl: url), //webView에 url을 집어넣어 해당링크의 페이지를 보여줍니다.
     );
   }
 }
